c2fa729e1777b947e61975b6d1cd23af7b2b3587
deepika <thotakuradeepikadevi@gmail.com>
2022-09-15 16:24:12 -0700

    task

--- demogit.py
+++ demogit.py
@@ -2,13 +2,11 @@
 # str = "deepika"
 # print("str")
 # print(len(str))
-
 # 2program
 # str=int(input("enter number"))
 # str1=float(input("enter number"))
 # print(str)
 # print(str1)
-
 # 3 type casting in dict
 # tup=("one",1),(2,"two")
 # print(tup)
@@ -16,62 +14,52 @@
 # d=dict(tup)
 # print(d)
 # print(type(d))
-
 # 4 covert list to tuple and vice versa
 # lst=[1,2,3,4,5,6]
 # lst1=tuple(lst)
 # print(lst1)
 # print(lst)
-
 # 5 after creating dict update dict by using keys and values
 # dict = {"name": "deepika", 23: "age", "nickname": "deepu,deepu"}
 # dict["percentage"] = 70
 # print(dict)
 # dict["percentage"] = 60
 # print(dict)
-
 # 6 difference b/w list & tuple
 # lst=[0,1,2,3,4]
 # lst.append(5) #list is mutable we can change the values in list
 # print(lst)
 # tup=(0,1,2,3,4)
 # tup.append(5) #attribute error because it's immutable
-
 # 7
 # str="Welcome to PYTHON"
 # print(str)
 # str2=(str[4:10])
 # print(str2)
-
 # 8 string in reverse order
 # str="DEEPU"
 # print(str)
 # str2=str[::-1]
 # print(str2)
-
 # 9 ordered list
 # lst=[10,5,4,20,1,15,12]
 # lst.sort()
 # print(lst)
-
 # 11)Write a program to reverse a string word by word.
 # str="sita ram"
 # str1=str.split()[::-1]
 # print(str1)
-
 # 13
 # l1=['a','b','c']
 # l2=[1,2,3]
 # l3=zip(l1,l2)
 # print(dict(l3))
-
-# 17 concatinate 3 dicts
+#17 concatinate 3 dicts
 # dic1={1:10,2:20}
 # dic2={3:30,4:40}
 # dic3={5:50,6:60}
 # dic4=dic1|dic2|dic3
 # print(dic4)
-
 # 14)Write a program to generate and print a list of the first and last 5 elements where the values
 # are square of numbers between 1 and 30 (both included)
 # l = []
@@ -82,13 +70,11 @@
 # l2=l[-5:]
 # print(l1)
 # print(l2)
-
 # 18)Write a program to demonstrate all the available operators in python.
 # a=13
 # b=20
 # c=a+b,a-b,a*b,a%b,a/b,a//b,a!=b,a==b,a>=b,a<=b,a<b,a>b
 # print(c)
-
 # 20
 # dict = {1: "one", 2: "two", 3: "three"}
 # dict1 = dict[3] = "four,five"
