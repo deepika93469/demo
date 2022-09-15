fa1731a3441a1b6f9dbf6bc8720d604049c95abe
deepika <thotakuradeepikadevi@gmail.com>
2022-09-15 13:38:03 -0700

    first demo commit

--- demogit.py
+++ demogit.py
@@ -1,5 +1,68 @@
-#calculate the length of the string
+# 1calculate the length of the string
 # str = "deepika"
 # print("str")
 # print(len(str))
+
+# 2program
+# str=int(input("enter number"))
+# str1=float(input("enter number"))
+# print(str)
+# print(str1)
+
+# 3 type casting in dict
+# tup=("one",1),(2,"two")
+# print(tup)
+# print(type(tup))
+# d=dict(tup)
+# print(d)
+# print(type(d))
+
+# 4 covert list to tuple and vice versa
+# lst=[1,2,3,4,5,6]
+# lst1=tuple(lst)
+# print(lst1)
+# print(lst)
+
+# 5 after creating dict update dict by using keys and values
+# dict = {"name": "deepika", 23: "age", "nickname": "deepu,deepu"}
+# dict["percentage"] = 70
+# print(dict)
+# dict["percentage"] = 60
+# print(dict)
+
+# 6 difference b/w list & tuple
+# lst=[0,1,2,3,4]
+# lst.append(5) #list is mutable we can change the values in list
+# print(lst)
+# tup=(0,1,2,3,4)
+# tup.append(5) #attribute error because it's immutable
+
+# 7
+# str="Welcome to PYTHON"
+# print(str)
+# str2=(str[4:10])
+# print(str2)
+
+# 8 string in reverse order
+# str="DEEPU"
+# print(str)
+# str2=str[::-1]
+# print(str2)
+
+# 9 ordered list
+# lst=[10,5,4,20,1,15,12]
+# lst.sort()
+# print(lst)
+
+# 13
+# l1=['a','b','c']
+# l2=[1,2,3]
+# l3=zip(l1,l2)
+# print(dict(l3))
+
+
+# 20
+# dict = {1: "one", 2: "two", 3: "three"}
+# dict1 = dict[3] = "four,five"
+# print(dict)
 
